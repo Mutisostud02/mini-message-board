9b5ed3bda639c925a2396ab1ad1fff1440d32c56
Mutisostud02 <mutiso8395@gmail.com>
2025-08-04 10:25:44 +0300

    Impoted logic from controllers to use

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const currentDate = new Date().toLocaleString();
-const messages = [
-  {
-    id: 1,
-    text: "Unakam saa ngapi",
-    user: "Love",
-    added: currentDate,
-  },
-  {
-    id: 2,
-    text: "Rada, alafu niliwai ile shugli",
-    user: "Adwerah",
-    added: currentDate,
-  }
-];
 
-async function getUserById(userId) {
-  return messages.find(message => message.id === userId)
-
-}
+const { getUser, getUserById, messages } = require('../controllers/indexController.js')
 
 router.get('/', (req, res) => {
     res.render("index", {title: "Mini Message App", messages: messages, href: "/new"})
@@ -31,16 +13,6 @@ router.post('/new', (req, res) => {
     res.redirect("/")
 })
 
-async function getUser(req, res) {
-  const { id } = req.params;
-  const user = await getUserById(Number(id));
-  if(!user) {
-    res.status(404).send('User Not Found!')
-    return;     
-  }
-  res.render("viewMessage", {text: user.text, user: user.user, added: user.added, href: `/${user.id}`})
-  
-}
 
 router.get('/:id', getUser)
 
